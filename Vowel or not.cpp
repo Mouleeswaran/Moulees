143b11fc3f672de281545a26f05b71962c5e386d
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-02-14 16:14:12 +0530

    Create Vowel or not.cpp

---
+++ Vowel or not.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char ch;
+	cout<<"Ente an alphabet\n";
+	cin>>ch;
+	switch(ch)
+	{
+		case 'a':
+		cout<<"The Albhabet is a Vowel";
+		break;
+		case 'e':
+		cout<<"The Albhabet is a Vowel";
+		break;
+		case 'i':
+		cout<<"The Albhabet is a Vowel";
+		break;
+		case 'o':
+		cout<<"The Albhabet is a Vowel";
+		break;
+		case 'u':
+		cout<<"The Albhabet is a Vowel";
+		break;
+		default:
+		cout<<"The Alphabet is not a Vowel";
+	}
+	return 0;
+}
