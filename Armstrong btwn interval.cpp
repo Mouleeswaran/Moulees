36d9dbe0f2f7c81b3e1fa271bae87378e0c4a5f9
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-02-15 14:50:45 +0530

    Update Armstrong btwn interval.cpp

--- Armstrong btwn interval.cpp
+++ Armstrong btwn interval.cpp
@@ -2,24 +2,27 @@
 using namespace std;
 
 int main() {
-	int num1,num2,temp,digit,sum,i;
+	int num1,num2,temp,sum,digit;
 	cout<<"Enter two numbers\n";
 	cin>>num1>>num2;
 	while(num1<num2)
 	{
-		num1=temp;
-		sum=0;
-		while(temp!=0)
-		{
-			digit=temp%10;
-			sum+=digit*digit*digit;
-			temp=temp/10;
-		}
-		if(num1==sum)
-		{
-			cout<<num1;
-		}
-		num1++;
+	temp=num1;
+	sum=0;
+	while(temp!=0)
+	{
+		digit=temp%10;
+		sum+=digit*digit*digit;
+		temp=temp/10;
+	}
+	if(num1==sum)
+	{	cout<<num1;
+	}
+	else
+	{
+		cout<<"No ARMSTRONG";
+	}
+	num1++;
 	}
 	return 0;
 }
