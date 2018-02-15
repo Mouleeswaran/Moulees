2203ac3fabcf34cf9203005a2f1d350680026e26
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-02-15 10:08:07 +0530

    Update Greater of 3.cpp

--- Greater of 3.cpp
+++ Greater of 3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	int num1,num2,num3;
-	cout<<"Enter three positive integer\n";
+	cout<<"Enter three integer\n";
 	cin>>num1>>num2>>num3;
 	if(num1>num2&&num1>num3)
 	cout<<"num1 is greater";
