2a2137497a0d85b4caacdefe47107ca121c5fd23
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-02-15 10:26:36 +0530

    Create print n times.cpp

---
+++ print n times.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char str[10]="HELLO";
+	int n,i;
+	cout<<"Enter a number\n";
+	cin>>n;
+	for(i=0;i<n;i++)
+	cout<<str<<"\n";
+	return 0;
+}
