97ef94c997daa131ad2f66bed81762f4ffe83de9
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-02-15 09:40:53 +0530

    Create Alphabet or not.cpp

---
+++ Alphabet or not.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char ch,;
+	int a;
+	cout<<"Enter a Character";
+	cin>>ch;
+	a=int(ch);
+	if((a<41!)!(a>90)&&(a<97)!!(a>122))
+	{
+		cout<<"ALPHABET";
+	}
+	else 
+	{
+		cout<<"NO";
+	}
+	return 0;
+}
