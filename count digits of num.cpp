4f8d09646046d97954a19f8b09069bae41806ea6
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-03-03 19:15:53 +0530

    Create count digits of num.cpp

---
+++ count digits of num.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num,temp,count=0;
+	cout<<"Enter the number\n";
+	cin>>num;
+	while(num=!0)
+	{
+		num=num/10;
+		count+=1;
+	}
+	cout<<count;
+	return 0;
+}
