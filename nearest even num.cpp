34a36c9ff1de19b1bc3d31daeadad786ed588daa
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-03-03 21:27:46 +0530

    Create nearest even num.cpp

---
+++ nearest even num.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num,result;
+	cout<<"Enter a number\n";
+	cin>>num;
+	if(num%2==0)
+	cout<<(num-2);
+	else
+	cout<<(num-1);
+	return 0;
+}
