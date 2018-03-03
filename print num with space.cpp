68c416880c8c62b8af47c5604732db277b50b6a2
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-03-03 20:46:49 +0530

    Create print num with space.cpp

---
+++ print num with space.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num,num1=0;
+	cout<<"Enter a numer\n";
+	cin>>num;
+	while(num=!0)
+	{
+	    num1=(num1*10)+(num%10);
+	    num=num/10;
+	}
+	while(num=!0)
+	{
+	    num=num1%10;
+	    cout<<num<<" ";
+	    num1=num1/10;
+	}
+	return 0;
+}
