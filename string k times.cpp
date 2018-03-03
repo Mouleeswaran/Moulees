4d6b48d5d955714cff08d3719883cd18c4beb972
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-03-03 18:48:11 +0530

    Create string k times.cpp

---
+++ string k times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char s[100];
+	int k;
+	cout<<"Enter the String\n";
+	cin>>s;
+	cout<<"Enter a number\n";
+	cin>>k;
+	for(int i=0;i<k;i++)
+	{
+		cout<<s<<"\n";
+	}
+	return 0;
+}
