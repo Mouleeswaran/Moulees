89588ae23684c207195ab88f1ef31ef4b78856d1
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-03-03 19:04:30 +0530

    Create check within range.cpp

---
+++ check within range.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cout<<"Enter a number\n";
+	cin>>num;
+	if(num<=10)
+	{
+	cout<<"YES";
+	}
+	else
+	{
+	cout<<"NO";
+	}
+	return 0;
+}
