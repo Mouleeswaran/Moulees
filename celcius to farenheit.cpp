5fd2620c44d77d035141d4864f30ea957011f769
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-03-09 21:51:18 +0530

    Create celcius to farenheit.cpp

---
+++ celcius to farenheit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double celcius,farenheit;
+	cout<<"Enter celcius\n";
+	cin>>celcius;
+	farenheit=celcius*1.8+32;
+	cout<<farenheit;
+	return 0;
+}
