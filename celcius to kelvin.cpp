389cfa8da1ac9d22233ac4787e99ce139f2e81bb
Mouleeswaran <36472711+Mouleeswaran@users.noreply.github.com>
2018-03-09 21:54:12 +0530

    Create celcius to kelvin.cpp

---
+++ celcius to kelvin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double celcius,kelvin;
+	cout<<"Enter celcius\n";
+	cin>>celcius;
+	kelvin=celcius+273.15;
+	cout<<kelvin;
+	return 0;
+}
